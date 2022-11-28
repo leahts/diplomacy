ce11dfc8f5858ba2bc981dd46317700af87a1f15
leahts <leah.elizabeth.smith@gmail.com>
2022-11-28 12:08:31 -0500

    Organized files into folders and changed file path

--- graph/diplomacy_graph.py
+++ graph/diplomacy_graph.py
@@ -5,29 +5,30 @@ Created on Wed Nov 23 19:55:17 2022
 
 @author: katherinesweeney
 """
-#Imports
+# Imports
 import networkx as nx
 import matplotlib.pyplot as plt
-import visualizing_graph
+import visualizing_graph as visualizing_graph
 
 
-#Class Nodes creates the nodes of the graph (i.e. the territories)
+# Class Nodes creates the nodes of the graph (i.e. the territories)
 class Node():
-    
+
     def __init__(self, territory_name, land_type, dot_status, neighbors):
         self.name = territory_name
         self.land_type = land_type
         self.dot_status = dot_status
         self.neighbors = neighbors
-    
+
     def printing(self):
-        print("The territory {} has neighbors {} and a dot status of {}".format(self.name, self.neighbors, self.dot_status))
+        print("The territory {} has neighbors {} and a dot status of {}".format(
+            self.name, self.neighbors, self.dot_status))
 
 
-#Main Body 
+# Main Body
 
-#open the graph file and parse data
-graph_file = open("map_data.csv", "r")
+# open the graph file and parse data
+graph_file = open("data\map_data.csv", "r")
 graph_file = graph_file.readlines()[1:]
 for line in graph_file:
     line = line.replace("\n", "")
@@ -36,7 +37,5 @@ for line in graph_file:
     node = Node(line[0], line[2], line[3], line[4])
     print_statement = node.printing()
 
-#visualize the graph
+# visualize the graph
 visualizing_graph.create_graph(graph_file)
-
-
