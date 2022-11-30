f74b0f615c7ce65661bacba0d9ad4f4082bd85e1
katherine-m-sweeney <118930050+katherine-m-sweeney@users.noreply.github.com>
2022-11-30 16:48:12 -0500

    map_data has (x, y) coordinates of each territory

--- gui/creating_gui.py
+++ gui/creating_gui.py
@@ -3,9 +3,10 @@ import io
 import os
 import PySimpleGUI as sg
 from PIL import Image
-#from pynput.mouse import Controller
+import cv2
 
 #Functions
+
 #Display image function: inputs a .png file and outputs the image
 def display_image(file):
     map_layout = [
@@ -17,17 +18,57 @@ def display_image(file):
         event, values = window.Read()
         if event == sg.WIN_CLOSED:
             break
-        """elif 
-            print("YES")
-            if os.path.exists(file):
+        """if os.path.exists(file):
                 image = Image.open(file)
                 print("yes")
                 image.resize((200, 200))
                 window[file].update()"""
         window.close()
 
-
+#From https://www.geeksforgeeks.org/displaying-the-coordinates-of-the-points-clicked-on-the-image-using-python-opencv/
+#Thank you geeksforgeeks
+def click_event(event, x, y, flags, params):
+    # checking for left mouse clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(x) + ',' +
+                    str(y), (x,y), font,
+                    1, (255, 0, 0), 2)
+        cv2.imshow('image', img)
+    # checking for right mouse clicks    
+    if event==cv2.EVENT_RBUTTONDOWN:
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv2.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r),
+                    (x,y), font, 1,
+                    (255, 255, 0), 2)
+        cv2.imshow('image', img)
+ 
+    
 #Main Body
-#thank you Kamran for the map
+#Thank you Kamran for the map
 image_file = "data/kamrans_map.png"
 display_image(image_file)
+img = cv2.imread(image_file, 1)
+
+
+"""
+#only used to get coordinates; from https://www.geeksforgeeks.org/displaying-the-coordinates-of-the-points-clicked-on-the-image-using-python-opencv/
+cv2.imshow('image', img)
+cv2.setMouseCallback('image', click_event)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
