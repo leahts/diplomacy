fcf0625551eda20b58fe49b9598e1a8ca56bd0bd
katherine-m-sweeney <118930050+katherine-m-sweeney@users.noreply.github.com>
2022-11-30 15:30:05 -0500

    updated map and gui

--- gui/creating_gui.py
+++ gui/creating_gui.py
@@ -3,12 +3,13 @@ import io
 import os
 import PySimpleGUI as sg
 from PIL import Image
+#from pynput.mouse import Controller
 
 #Functions
 #Display image function: inputs a .png file and outputs the image
 def display_image(file):
     map_layout = [
-        [sg.Image(file, size =(700, 700))],
+        [sg.Image(file, size =(600, 400))],
         [sg.Text("Imperial Diplomacy")]
     ]
     window = sg.Window("Imperial Diplomacy: Spring 1900", map_layout)
@@ -25,6 +26,8 @@ def display_image(file):
                 window[file].update()"""
         window.close()
 
+
 #Main Body
+#thank you Kamran for the map
 image_file = "data/kamrans_map.png"
 display_image(image_file)
