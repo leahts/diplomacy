f5bf51e733abc02e82dc7981622febe6a6deb63d
katherine-m-sweeney <118930050+katherine-m-sweeney@users.noreply.github.com>
2022-12-08 13:42:06 -0500

    fixed Stp - added Stp-NC and Stp-SC

--- units/creating_units.py
+++ units/creating_units.py
@@ -4,17 +4,24 @@ from PIL import ImageDraw
 def unit_color(country):
     if country == "Austria":
         color = (255, 0, 0)
+    return color
 
-def create_unit(territory, country, image_file):
+def create_unit(territory, unit_type, country, image_file):
+    black = (0, 0, 0, 255)
     color = unit_color(country)
     image = Image.open(image_file)
     x_pos = territory[0]
     y_pos = territory[1]
     x_pos = int(x_pos)
     y_pos = int(y_pos)
-    ellipse_dimensions = [(x_pos - 5, y_pos - 5), (x_pos + 5, y_pos + 5)]
-    draw = ImageDraw.Draw(image, "RGBA")
-    draw.ellipse(ellipse_dimensions, outline = (0, 0, 0, 255), fill = color)
+    if unit_type == "Army":
+        ellipse_dimensions = [(x_pos - 5, y_pos - 5), (x_pos + 5, y_pos + 5)]
+        draw = ImageDraw.Draw(image, "RGBA")
+        draw.ellipse(ellipse_dimensions, outline = black, fill = color)
+    elif unit_type == "Fleet":
+        triangle_points = [(x_pos, y_pos + 7), (x_pos - 6, y_pos - 4), (x_pos + 6, y_pos - 4)]
+        draw = ImageDraw.Draw(image, "RGBA")
+        draw.polygon(triangle_points, outline = black, fill = color)
     image.save("data/map_w_units.png", format = "png")
     updated_map = "data/map_w_units.png"
     return updated_map
